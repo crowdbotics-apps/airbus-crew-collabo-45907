71efd3147bb9ebf8e5ba0b6c3661621825fb262b
Crowdbotics <team@crowdbotics.com>
2024-02-05 15:46:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "airbus-crew-collabo-45907",
+  url: "https://airbus-crew-collabo-45907.botics.co",
+  api: "https://airbus-crew-collabo-45907.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
